1aec7d7ec83f1ce346313f20a2cf8c72e3418a0e
tempesttswinson <tempestt.swinson@gmail.com>
2020-08-13 14:34:04 -0400

    styling added to home page

--- ATAKA_Organix/src/main/frontend/src/app/contactus/contactus.component.ts
+++ ATAKA_Organix/src/main/frontend/src/app/contactus/contactus.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { } from 'googlemaps';
 
 @Component({
   selector: 'app-contactus',
@@ -11,15 +12,25 @@ export class ContactusComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(function () {
+      function init_map1() {
+        var myLocation = new google.maps.LatLng(38.885516, -77.09327200000001);
+        var mapOptions = {
+          center: myLocation,
+          zoom: 16
+        };
+        var marker = new google.maps.Marker({
+          position: myLocation,
+          title: "Property Location"
+        });
+        var map = new google.maps.Map(document.getElementById("map1"),
+          mapOptions);
+        marker.setMap(map);
+      }
+      init_map1();
+
+    });
   }
 
-  //   let map: google.maps.Map;
-
-  // function initMap(): void {
-  //   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  //     center: { lat: -34.397, lng: 150.644 },
-  //     zoom: 8
-  //   });
-  // }
 
 }
